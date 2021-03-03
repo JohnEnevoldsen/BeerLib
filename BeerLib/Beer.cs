70fb47b4f2b65db795adb6d5d65b088aad8e4c1e
JohnEnevoldsen <62882814+JohnEnevoldsen@users.noreply.github.com>
2021-03-03 13:47:23 +0100

    Add project files.

---
+++ BeerLib/Beer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace BeerLibrary
+{
+    public class Beer
+    {
+        private int _id;
+        private string _name;
+        private int _price;
+        private double _abv;
+
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value.Length < 4)
+                {
+                    throw new ArgumentException();
+                }
+                _name = value;
+            }
+        }
+        public int Price
+        {
+            get { return _price; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                _price = value;
+            }
+        }
+        public double Abv
+        {
+            get { return _abv; }
+            set
+            {
+                if (value < 0 || value > 100)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                _abv = value;
+            }
+        }
+
+        public Beer(string name, int price, double abv)
+        {
+            if (name.Length < 4)
+            {
+                throw new ArgumentException();
+            }
+            _name = name;
+            if (price <= 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            _price = price;
+            if (abv < 0 || abv > 100)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            _abv = abv;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ BeerLibTests6/BeerTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BeerLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeerLibrary.Tests
+{
+    [TestClass()]
+    public class BeerTests
+    {
+        Beer beer;
+
+        [TestInitialize]
+        public void BeerInitialize()
+        {
+            beer = new Beer("Turbog", 74, 4.6);
+        }
+
+        [TestMethod()]
+        public void BeerTest()
+        {
+            Assert.ThrowsException<ArgumentException>(() => new Beer("Tre", 70, 70));
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Beer("Fire", 0, 70));
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Beer("Fire", 70, -3));
+        }
+        [TestMethod]
+        public void NameTest()
+        {
+            Assert.ThrowsException<ArgumentException>(() => beer.Name = "Tur");
+            beer.Name = "John";
+            Assert.AreEqual("John", beer.Name);
+        }
+        [TestMethod]
+        public void PriceTest()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => beer.Price = -45);
+            beer.Price = 37;
+            Assert.AreEqual(37, beer.Price);
+        }
+        [TestMethod]
+        public void ABVTest()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => beer.Abv = -2.2);
+            beer.Abv = 1.0;
+            Assert.AreEqual(1, beer.Abv);
+        }
+    }
+}
